820727f2a7c9489d236b29b09ae9e552b0474482
Octaver <Octaver2131@outlook.com>
2025-06-12 14:07:51 +0800

    menu

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="globalHeader">
+    <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from "vue";
+import { HomeOutlined, CrownOutlined } from "@ant-design/icons-vue";
+import { MenuProps } from "ant-design-vue";
+const current = ref<string[]>(["mail"]);
+const items = ref<MenuProps["items"]>([
+  {
+    key: "/",
+    icon: () => h(HomeOutlined),
+    label: "主页",
+    title: "主页",
+  },
+  {
+    key: "/user/login",
+    label: "用户登入",
+    title: "用户登入",
+  },
+  {
+    key: "/user/register",
+    label: "用户注册",
+    title: "用户注册",
+  },
+  {
+    key: "/admin/manage",
+    icon: () => h(CrownOutlined),
+    label: "用户管理",
+    title: "用户管理",
+  },
+  {
+    key: "others",
+    label: h("a", { href: "https://github.com/Octaver2131" }, "联系欧可可"),
+    title: "GitHub",
+  },
+]);
+</script>
