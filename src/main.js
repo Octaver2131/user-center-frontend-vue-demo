d4ce2205a751542fea7bab09d311b73cdfd28a5b
Octaver <Octaver2131@outlook.com>
2025-06-13 10:20:09 +0800

    微调

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import { createPinia } from "pinia";
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/reset.css";
-const pinia = createPinia();
-createApp(App).use(pinia).use(Antd).use(router).mount("#app");
-//# sourceMappingURL=main.js.map
